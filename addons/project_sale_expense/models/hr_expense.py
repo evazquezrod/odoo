938cf0543592e86bf23ffa837185534d6e5db0d7
Logan Staelens <lost@odoo.com>
2025-02-11 17:32:12 +0100

    [IMP] WIP

--- addons/project_sale_expense/models/hr_expense.py
+++ addons/project_sale_expense/models/hr_expense.py
@@ -10,7 +10,25 @@ class Expense(models.Model):
     def _compute_analytic_distribution(self):
         super()._compute_analytic_distribution()
         if not self.env.context.get('project_id'):
+            expenses_to_recompute = self.env['hr.expense']
+            prefetch_ids = set()
             for expense in self:
                 if not self.sale_order_id:
                     continue
-                expense.analytic_distribution = expense.sale_order_id.project_id._get_analytic_distribution()
+                expenses_to_recompute += expense
+                prefetch_ids.update(list(expense.analytic_distribution.keys()))
+
+            if expenses_to_recompute:
+                analytic_account_model = self.env['account.analytic.account'].with_prefetch(prefetch_ids)
+                for expense in expenses_to_recompute:
+                    project_analytic_distribution = expense.sale_order_id.project_id._get_analytic_distribution()
+                    project_analytic_distribution_accounts = self.env['account.analytic.account'].browse(list(project_analytic_distribution.keys()))
+
+                    analytic_accounts = analytic_account_model.browse(list(expense.analytic_distribution.keys()))
+                    if expense.analytic_distribution:
+                        expense.analytic_distribution = {
+                            **expense.analytic_distribution,
+                            **project_analytic_distribution
+                        }
+                    else:
+                        expense.analytic_distribution = expense.sale_order_id.project_id._get_analytic_distribution()
