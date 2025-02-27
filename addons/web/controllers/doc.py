044633cc190cf08d0a2e7307700030667e8d8022
Julien Castiaux <juc@odoo.com>
2025-02-27 19:11:09 +0100

    wip

--- addons/web/controllers/doc.py
+++ addons/web/controllers/doc.py
@@ -4,29 +4,52 @@ import inspect
 from werkzeug.exceptions import NotFound
 
 from odoo import http
+from odoo.exceptions import AccessError
 from odoo.http import request
+from odoo.service.model import get_public_method
+
+
+def is_public_method(model, name):
+    try:
+        get_public_method(model, name)
+    except (AttributeError, AccessError):
+        return False
+    else:
+        return True
 
 
 class DocController(http.Controller):
-    @http.route([
-        '/doc',
-        '/doc/<model>',
-        '/doc/<model>.html',
-        '/doc/<model>.json'
-    ], type='http', auth='public', readonly=True)
-    def doc(self, model=None):
-        if not model:
-            return request.redirect('/doc/res.partner.json')
-        if not request.httprequest.path.endswith(('.html', '.json')):
-            request.redirect(request.httprequest.path + '.html')
+    @http.route('/doc', type='http', auth='public', readonly=True)
+    def doc_home(self, model=None):
+        return (
+            "<p>Page under construction<p>"
+            "<p>Please go to <a href={0!r}>{0}</a></p>"
+        ).format(f'/doc/{model or 'base'}.json')
 
+    @http.route('/doc/schema.json', type='http', auth='public', readonly=True)
+    def doc_schema(self):
+        schema = {}
+        for model in request.env:
+            fields = model.fields_get()
+            schema[model] = {
+                'fields': list(fields),
+                'methods': [
+                    attr
+                    for attr in dir(model)
+                    if attr not in fields
+                    if is_public_method(attr)
+                ],
+            }
+
+        return request.make_json_response(schema)
+
+    @http.route('/doc/<model>.json', type='http', auth='public', readonly=True)
+    def doc_model(self, model):
         if model not in request.env:
             m = f"Model {model!r} not found."
             if closest5 := difflib.get_close_matches(model, request.env, 5):
                 m += f"\n\nClose matches include:\n- {f'{chr(10)}- '.join(closest5)}"
             raise NotFound(m)
-
-        # TODO: use the registry sequence as ETag, for caching
 
         Model = request.env[model]
         fields = Model.fields_get()
@@ -40,14 +63,8 @@ class DocController(http.Controller):
             if not getattr(meth, '_api_private', False)
         }
 
-        if request.httprequest.path.endswith('.json'):
-            return request.make_json_response({
-                'fields': fields,
-                'methods': public_methods
-            })
-
-        # we should render a template
-        return (
-            "<p>Page under construction<p>"
-            "<p>Please go to <a href={0!r}>{0}</a></p>"
-        ).format(request.httprequest.path + '.json')
+        # TODO: OpenAPI
+        return request.make_json_response({
+            'fields': fields,
+            'methods': public_methods
+        })
