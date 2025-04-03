f3db3834516979e52f20718c2b0ad27301f688b1
Loan (LSE) <lse@odoo.com>
2025-04-03 17:46:12 +0200

    [IMP] hw_*: IoT compatibility for Linux/Mac computers
    Testing IoT flows is tricky due to the hardware constraints.
    The goal of this PR is **NOT** to propose a new kind of IoT but
    rather an easier way to run IoT's code on a Linux/Mac OS environment.
    
    It is plan to add more unit tests using a Linux environment to use
    real IoT interactions to better cover IoT's code.
    
    Current focus of this PR is to be able to run an odoo regular
    instance alongside IoT's code.
    This needed to adapt some part of IoT's code as IoT is not meant
    to run with a database.
    
    Example of run with IoT code locally with a database (assuming to
    be on a Linux computer with a regular odoo setup installation):
    1. Make sure to have installed IoT requirements with:
    `pip3 install -r <odoo>/addons/iot_box_image/configuration/requirements-local.txt`
    2. Run odoo and explicitely load IoT related modules
    `odoo-bin --load=hw_drivers,hw_posbox_homepage -d test-with-iot`
    
    Note:
     - To access IoT's homepage, you can use the route:
     /hw_posbox_homepage/
     - .gitignore added in hw_drivers/iot_handlers folder to avoid
     accidentally pushing modified/added handlers downloaded

--- addons/hw_drivers/tools/helpers.py
+++ addons/hw_drivers/tools/helpers.py
@@ -128,6 +128,59 @@ def start_nginx_server():
         subprocess.check_call(["sudo", "service", "nginx", "restart"])
 
 
+@toggleable
+@require_db
+def check_git_branch(server_url=None):
+    """Check if the local branch is the same as the connected Odoo DB and
+    checkout to match it if needed.
+
+    :param server_url: The URL of the connected Odoo database (provided by decorator).
+    """
+    try:
+        response = requests.post(server_url + "/web/webclient/version_info", json={}, timeout=5)
+        response.raise_for_status()
+        data = response.json()
+    except requests.exceptions.HTTPError:
+        _logger.exception('Could not reach configured server to get the Odoo version')
+        return
+    except ValueError:
+        _logger.exception('Could not load JSON data: Received data is not valid JSON.\nContent:\n%s', response.content)
+        return
+
+    try:
+        git = ['git', '--work-tree=/home/pi/odoo/', '--git-dir=/home/pi/odoo/.git']
+
+        db_branch = data['result']['server_serie'].replace('~', '-')
+        if not subprocess.check_output(git + ['ls-remote', 'origin', db_branch]):
+            db_branch = 'master'
+
+        local_branch = (
+            subprocess.check_output(git + ['symbolic-ref', '-q', '--short', 'HEAD']).decode('utf-8').rstrip()
+        )
+        _logger.info(
+            "Current IoT Box local git branch: %s / Associated Odoo database's git branch: %s",
+            local_branch,
+            db_branch,
+        )
+
+        if db_branch != local_branch:
+            try:
+                with writable():
+                    subprocess.run(git + ['branch', '-m', db_branch], check=True)
+                    subprocess.run(git + ['remote', 'set-branches', 'origin', db_branch], check=True)
+                    _logger.info("Updating odoo folder to the branch %s", db_branch)
+                    subprocess.run(
+                        ['/home/pi/odoo/addons/iot_box_image/configuration/checkout.sh'], check=True
+                    )
+            except subprocess.CalledProcessError:
+                _logger.exception("Failed to update the code with git.")
+            finally:
+                odoo_restart()
+    except Exception:
+        _logger.exception('An error occurred while trying to update the code with git')
+
+>>>>>>> d094e03b6d9f ([IMP] hw_*: IoT compatibility for Linux/Mac computers)
+
 def check_image():
     """Check if the current image of IoT Box is up to date
 
