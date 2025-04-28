8ebf820a9b828a846ada275e50e9b16c747fe491
Nicolas Viseur (vin) <vin@odoo.com>
2025-04-28 08:44:49 +0800

    [IMP] PH

--- addons/l10n_ph/__manifest__.py
+++ addons/l10n_ph/__manifest__.py
@@ -20,6 +20,7 @@
         'views/account_move_views.xml',
         'views/account_payment_views.xml',
         'views/account_tax_views.xml',
+        'views/res_company_views.xml',
         'views/res_partner_views.xml',
         'security/ir.model.access.csv',
     ],
