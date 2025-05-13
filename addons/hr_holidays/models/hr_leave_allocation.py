d751013d7c6793a044db86b006d04f6c4c2f5884
Youssef Bashandy <yoba@odoo.com>
2025-05-13 09:49:27 +0200

    [FIX] hr_holidays : Correctly calculate future accrual allocation
    Steps to reproduce:
    	- Create an accrual plan with the following rule:
    		— The employee has 20 days off in the first year. Total 20.
    		— The employee has 21 days off in the second year and an additional 5 days off if available from the previous year, which can be taken until 6 months. Total 21 + 5 = 26
    		— The employee has 22 days off in the third year and an additional 5 days off if available from the previous year, which can be taken until 6 months. Total 22 + 5 = 27
    		— The employee has 23 days off in the fourth year and an additional 5 days off if available from the previous year, which can be taken until 6 months. Total 23 + 5 = 28
    	- Create an accrual allocation with the created plan starting 01/01/2025
    	- Check future allocation data using 'Balance at the'
    	- Notice the following behaviour:
    		— until 31/12/2025 it CORRECTLY shows 20 days available.
    
    		— from 01/01/2026 to 30/06/2026 it CORRECTLY shows 26 days (21 days for renewal and 5 days not used in 2025)
    		— from 01/07/2026 it INCORRECTLY shows no days available.
    
    		— from 01/01/2027 to 30/06/2027 it CORRECTLY shows 27 days (22 days for renewal and 5 days not used in 2026)
    		— from 01/07/2027 it INCORRECTLY shows 5 days.
    
    		— from 01/01/2028 to 30/06/2028 it CORRECTLY shows 28 days (23 days for renewal and 5 days not used in 2027)
    		— from 01/07/2028 it INCORRECTLY shows no days available.
    
    		— from 01/01/2029 it CORRECTLY shows 28 days again.
    		— In the following years, after 6 months, one year shows 5 days and the next shows nothing.
    
    Cause:
    The first cause here is that when we have validity for the carryover then
    we will have two calls in each year one at the start of the year and another
    at the expiration date of the carryover. So, when we add the days to the allocation
    we don't consider the second call in the condition and we only check if the
    allocation.actual_lastcall is equal to one of the start dates for each year
    
    https://github.com/odoo/odoo/blob/3fb37cbc59adc2caace8efcdae418d2466a9b750/addons/hr_holidays/models/hr_leave_allocation.py#L588
    
    The second cause where each two years one of them shows the number of
    carryover days from the previous year, this is happening because when we
    remove the expiring days for the first year we set the number of days to 0
    
    https://github.com/odoo/odoo/blob/3fb37cbc59adc2caace8efcdae418d2466a9b750/addons/hr_holidays/models/hr_leave_allocation.py#L516-L517
    
    And it will be 0 until we loop again and add the days to allocation
    
    https://github.com/odoo/odoo/blob/3fb37cbc59adc2caace8efcdae418d2466a9b750/addons/hr_holidays/models/hr_leave_allocation.py#L521-L522
    
    and since this is happening after we already set the expiring days which in
    this year will be 0 we won't remove those expiring days from the year's allocation
    data
    
    Fix:
    We add a condition to check if the actual_lastcall is either a date in the start of the allocation
    or one of the expiration dates for the carryover.
    
    Also, before we set the value of the expiring_carryover_days we call
    _add_days_to_allocation to calculate on the correct number of days for the
    plan level we are checking.
    
    opw-4606886

--- addons/hr_holidays/models/hr_leave_allocation.py
+++ addons/hr_holidays/models/hr_leave_allocation.py
@@ -341,7 +341,7 @@ class HolidaysAllocation(models.Model):
         current_level = False
         current_level_idx = -1
         for idx, level in enumerate(level_ids):
-            if date > self.date_from + get_timedelta(level.start_count, level.start_type):
+            if date >= self.date_from + get_timedelta(level.start_count, level.start_type):
                 current_level = level
                 current_level_idx = idx
         # If transition_mode is set to `immediately` or we are currently on the first level
@@ -507,6 +507,8 @@ class HolidaysAllocation(models.Model):
                 if allocation.nextcall == carryover_date:
                     allocation.last_executed_carryover_date = carryover_date
                     if current_level.action_with_unused_accruals in ['lost', 'maximum']:
+                        if current_level != first_level or (nextcall == expiration_date and allocation.number_of_days - leaves_taken == 0):
+                            allocation._add_days_to_allocation(current_level, current_level_maximum_leave, leaves_taken, period_start, period_end)
                         allocated_days_left = allocation.number_of_days - leaves_taken
                         allocation_max_days = 0 # default if unused_accrual are lost
                         if current_level.action_with_unused_accruals == 'maximum':
@@ -587,7 +589,9 @@ class HolidaysAllocation(models.Model):
                         current_level_maximum_leave = current_level.maximum_leave
                     else:
                         current_level_maximum_leave = current_level.maximum_leave / allocation.employee_id._get_hours_per_day(allocation.date_from)
-                if allocation.actual_lastcall in {period_start, allocation.date_from} | set(level_start.keys()):
+                if allocation.actual_lastcall in {period_start, allocation.date_from} | set(level_start.keys())\
+                        or (allocation.actual_lastcall - get_timedelta(current_level.accrual_validity_count, current_level.accrual_validity_type)
+                            in {period_start, allocation.date_from} | set(level_start.keys())):
                     allocation._add_days_to_allocation(current_level, current_level_maximum_leave, leaves_taken, period_start, allocation.nextcall)
                     allocation.already_accrued = True
 
