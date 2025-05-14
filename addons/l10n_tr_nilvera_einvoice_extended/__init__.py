b6c62e66d7d75831901b74e37b1eac21b702fa4c
Khumam ALKH <alkh@odoo.com>
2025-05-14 10:56:06 +0400

    [ADD] l10n_tr_nilvera_einvoice_extended, l10_tr_nilvera_einvoice: Adding nilvera einvoice extention [ADD] l10n_tr_nilvera_einvoice_extended: Adding Tax Offices
    This PR adds an extension module for Nilvera E-Invoicing. The purpose of this module is to extend the existing E-Invoicing functionality to support additional invoice types and scenarios to be compliant with the TR legal requirements. It also includes UBL XML changes to comply with the Nilvera e-invoicing.
    
    Task ID(s): 4345639, 4623689

---
+++ addons/l10n_tr_nilvera_einvoice_extended/__init__.py
@@ -0,0 +1,19 @@
+from . import models
+
+import logging
+
+_logger = logging.getLogger(__name__)
+
+
+def _l10n_tr_nilvera_einvoice_extended_post_init(env):
+    """Existing companies that have the Turkish Chart of Accounts set"""
+    template_codes = ["tr"]
+    tr_companies = env["res.company"].search(
+        [("chart_template", "in", template_codes), ("parent_id", "=", False)]
+    )
+    for company in tr_companies:
+        template_code = company.chart_template
+        ChartTemplate = env["account.chart.template"].with_company(company)
+        data = ChartTemplate._get_tr_withholding_account_tax()
+        ChartTemplate._deref_account_tags(template_code, data)
+        ChartTemplate._load_data({"account.tax": data})
---
+++ addons/l10n_tr_nilvera_einvoice_extended/models/template_tr.py
@@ -0,0 +1,14 @@
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+from odoo import models
+from odoo.addons.account.models.chart_template import template
+
+
+class AccountChartTemplate(models.AbstractModel):
+    _inherit = "account.chart.template"
+
+    @template("tr", "account.tax")
+    def _get_tr_withholding_account_tax(self):
+        additional = self._parse_csv(
+            "tr", "account.tax", module="l10n_tr_nilvera_einvoice_extended"
+        )
+        return additional
