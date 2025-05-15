ff6a0ee1f79af89d4acf9812aa87bbd0f5d07c8a
dija-odoo <dija@odoo.com>
2025-05-15 12:24:37 +0530

    [ADD] payment_worldline: backport to 17.0
    Legacy payment APIs will be discontinued by 31 Dec 2025.
    Backport Worldline provider from 18.0 to 17.0 to support early migration.
    
    Before this commit:
    - Worldline payment provider was only available in 18.0
    
    After this commit:
    - Worldline payment provider is backported to 17.0.
    - A smooth upgrade is ensured while preserving Ogone tokens,
      knowing Worldline already exist in 18.0,
    
    Affected version-17.0
    task-4687593

---
+++ addons/payment_worldline/__init__.py
@@ -0,0 +1,28 @@
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from . import controllers
+from . import models
+
+from odoo.addons.payment import setup_provider, reset_payment_provider
+
+
+def post_init_hook(env):
+    setup_provider(env, 'worldline')
+    _migrate_ogone_to_worldline(env)
+
+
+def uninstall_hook(env):
+    reset_payment_provider(env, 'worldline')
+
+
+def _migrate_ogone_to_worldline(env):
+    worldline_provider = env['payment.provider'].search([('code', '=', 'worldline')])
+    ogone_provider = env['payment.provider'].search([('code', '=', 'ogone')], limit=1)
+    if ogone_provider:
+        ogone_tokens = env['payment.token'].search([
+            ('provider_id', '=', ogone_provider.id)
+        ])
+        if ogone_tokens:
+            ogone_tokens.write({'provider_id': worldline_provider.id})
+        ogone_provider.write({'state': 'disabled'})
+    env['account.payment.method'].search([('code', '=', 'ogone')]).unlink()
