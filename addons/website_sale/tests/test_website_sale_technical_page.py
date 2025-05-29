4c53dd6a7bb8819098e386df2531127758904545
assk-odoo <assk@odoo.com>
2025-05-29 11:57:00 +0530

    [IMP] website: list important customizable pages
    Before this commit:
    Users may struggle to edit specific pages as they were not
    listed anywhere. They had to manually find the editable pages,
    making it difficult to locate and edit important ones.
    
    After this commit:
    Users can view a list of important editable pages under
    `Site > Technical Pages`. This helps users easily discover,
    access, and edit these pages without searching for routes.
    To add a page to the list, developers need to add
    `list_as_editable_page` in the route definition.
    
    Enterprise : https://github.com/odoo/enterprise/pull/88902
    
    task-4712084

---
+++ addons/website_sale/tests/test_website_sale_technical_page.py
@@ -0,0 +1,19 @@
+from odoo.tests import tagged
+from odoo.addons.website.tests.test_website_technical_page import TestWebsiteTechnicalPage
+
+
+@tagged("post_install", "-at_install")
+class TestWebsiteSaleTechnicalPage(TestWebsiteTechnicalPage):
+
+    def test_load_website_sale_technical_pages(cls):
+        expected_routes = [
+            "/donation/pay",
+            "/payment/status",
+            "/shop",
+            "/shop/confirmation",
+            "/shop/extra_info",
+            "/shop/payment",
+            "/shop/checkout"
+        ]
+        for route in expected_routes:
+            cls.assertIn(route, cls.created_routes, f"Route '{route}' is missing from technical page records.")
