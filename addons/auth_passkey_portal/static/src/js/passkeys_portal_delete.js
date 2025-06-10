28ee93c0ea2010aaf75f15d1d57bf63865c7cb94
bram1000 <brvg@odoo.com>
2025-06-10 14:16:44 +0200

    [ADD] auth_passkey_portal: WIP

---
+++ addons/auth_passkey_portal/static/src/js/passkeys_portal_delete.js
@@ -0,0 +1,32 @@
+import { Interaction } from "@web/public/interaction";
+import { registry } from "@web/core/registry";
+import { handleCheckIdentity } from "@portal/interactions/portal_security";
+
+export class PortalPasskeyDelete extends Interaction {
+    static selector = ".o_passkey_dropdown a[name='delete']";
+    dynamicContent = {
+        _root: { "t-on-click": this.onClick }
+    }
+
+    setup() {
+        this.id = this.el.parentElement.parentElement.querySelector("input[name='id']").value
+    }
+
+    async onClick() {
+        this.deletePasskey()
+    }
+
+    async deletePasskey() {
+        await this.waitFor(handleCheckIdentity(
+            this.waitFor(this.services.orm.call("auth.passkey.key", "action_delete_passkey", [this.id])),
+            this.services.orm,
+            this.services.dialog,
+        ));
+        location.reload();
+    }
+}
+
+registry
+    .category("public.interactions")
+    .add("auth_passkey_portal.delete", PortalPasskeyDelete);
+
