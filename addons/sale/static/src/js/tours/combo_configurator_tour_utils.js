96eed5ed1f819c2211e7a0df7aae8963717c93f5
dere-odoo <dere@odoo.com>
2025-06-19 14:32:07 +0530

    [FIX] website_sale,sale: review improvements

--- addons/sale/static/src/js/tours/combo_configurator_tour_utils.js
+++ addons/sale/static/src/js/tours/combo_configurator_tour_utils.js
@@ -30,6 +30,7 @@ function assertComboCount(count) {
 }
 
 function assertComboItemCount(comboName, count) {
+
     return {
         content: `Assert that there are ${count} combo items in combo ${comboName}`,
         trigger: comboSelector(comboName),
@@ -55,6 +56,19 @@ function assertSelectedComboItemCount(count) {
     };
 }
 
+function assertPreselectedComboItemCount(count) {
+    return {
+        content: `Assert that there are ${count} selected combo items`,
+        trigger: '.sale-combo-configurator-dialog',
+        run() {
+            const selector = `.sale-combo-configurator-dialog div[name="preselected_product_name"]`;
+            if (queryAll(selector).length !== count) {
+                console.error(`Assertion failed`);
+            }
+        },
+    };
+}
+
 function selectComboItem(comboItemName) {
     return {
         content: `Select combo item ${comboItemName}`,
@@ -70,9 +84,9 @@ function assertComboItemSelected(comboItemName) {
     };
 }
 
-function assertComboItemPreSelected(comboItemName) {
+function assertComboItemPreselected(comboItemName) {
     return {
-        content: `Assert that combo item ${comboItemName} is pre selected`,
+        content: `Assert that combo item ${comboItemName} is preselected`,
         trigger: `[name="preselected_product_name"]:contains(${comboItemName})`,
     };
 }
@@ -181,9 +195,10 @@ export default {
     assertComboCount,
     assertComboItemCount,
     assertSelectedComboItemCount,
+    assertPreselectedComboItemCount,
     selectComboItem,
     assertComboItemSelected,
-    assertComboItemPreSelected,
+    assertComboItemPreselected,
     increaseQuantity,
     decreaseQuantity,
     setQuantity,
