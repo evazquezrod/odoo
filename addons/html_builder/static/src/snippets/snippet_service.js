0ef26161a3d2bf68a77881b73b4d20cfd6493f74
Serhii Rubanskyi <seru@odoo.com>
2025-06-23 14:03:18 +0200

    [IMP] html_builder: avoid unnecessary orm call on snippet renaming
    After this [commit] `load` function with orm call was performed only for
    the purpose of recomputing snippets templates, when only snippet names
    were changed, which is unnecessary.
    
    [commit]: odoo/odoo@544bb04

--- addons/html_builder/static/src/snippets/snippet_service.js
+++ addons/html_builder/static/src/snippets/snippet_service.js
@@ -179,9 +179,9 @@ export class SnippetModel extends Reactive {
                     [this.snippetsName, {}],
                     { context }
                 );
-                const snippetsDocument = new DOMParser().parseFromString(html, "text/html");
-                this.computeSnippetTemplates(snippetsDocument);
-                this.setSnippetName(snippetsDocument);
+                this.snippetsDocument = new DOMParser().parseFromString(html, "text/html");
+                this.computeSnippetTemplates(this.snippetsDocument);
+                this.setSnippetName(this.snippetsDocument);
             })();
         }
         return this.loadProm;
@@ -295,14 +295,17 @@ export class SnippetModel extends Reactive {
             return;
         }
         snippet.title = newName;
+        for (const snippetEl of this.snippetsDocument.body.querySelectorAll(
+            `snippets#snippet_custom > [data-oe-snippet-key = ${snippet.key}]`
+        )) {
+            snippetEl.setAttribute("name", newName);
+            snippetEl.children[0].dataset["name"] = newName;
+        }
         await this.orm.call("ir.ui.view", "rename_snippet", [], {
             name: newName,
             view_id: snippet.viewId,
             template_key: this.snippetsName,
         });
-        // Reload snippet to have updated name.
-        this.loadProm = null;
-        await this.load();
     }
 
     setSnippetName(snippetsDocument) {
