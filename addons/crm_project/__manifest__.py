3e85d93031e8c32e4697b7159f6a449bef5861b2
Abdullah (abye) <abye@odoo.com>
2025-06-27 17:15:08 +0200

    [ADD] crm_project: convert opportunity into a project
    menu button to create a project out of opportunity
    
    task-4847853

---
+++ addons/crm_project/__manifest__.py
@@ -0,0 +1,17 @@
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    "name": "CRM - Project",
+    "version": "1.0",
+    "summary": "Project Generation from Opportunities",
+    "depends": [
+        "project",
+        "crm",
+    ],
+    "data": [
+        "views/crm_lead_views.xml",
+    ],
+    "installable": True,
+    "author": "Odoo S.A.",
+    "license": "LGPL-3",
+}
