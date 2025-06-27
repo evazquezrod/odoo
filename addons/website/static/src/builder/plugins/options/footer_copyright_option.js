4a74520dc1a2ed6bf3b2de04261880bca364b0a0
Brieuc-brd <brd@odoo.com>
2025-06-27 14:00:47 +0200

    custom copyright depending on the footer template

--- addons/website/static/src/builder/plugins/options/footer_copyright_option.js
+++ addons/website/static/src/builder/plugins/options/footer_copyright_option.js
@@ -4,14 +4,28 @@ import { rpc } from "@web/core/network/rpc";
 
 export class FooterCopyrightOption extends BaseOptionComponent {
     static template = "website.FooterCopyrightOption";
-    static props = {};
+    static props = {
+        getCurrentActiveViews: Function,
+    };
 
     setup() {
         super.setup();
         this.languages = null;
+        this.currentActiveViews = {};
 
         onWillStart(async () => {
             this.languages = await rpc("/website/get_languages", {}, { cached: true });
+            this.currentActiveViews = await this.props.getCurrentActiveViews(this.keys);
         });
+    }
+
+    hasSomeViews(views) {
+        for (const view of views) {
+            // console.log(this.currentActiveViews[view])
+            if (this.currentActiveViews[view]) {
+                return true;
+            }
+        }
+        return false;
     }
 }
--- addons/website/static/src/builder/plugins/options/footer_copyright_option_plugin.js
+++ addons/website/static/src/builder/plugins/options/footer_copyright_option_plugin.js
@@ -4,6 +4,7 @@ import { FooterCopyrightOption } from "@website/builder/plugins/options/footer_c
 
 class FooterCopyrightOptionPlugin extends Plugin {
     static id = "footerCopyrightOption";
+    static dependencies = ["HeaderNavbarOptionPlugin", "customizeWebsite"];
 
     resources = {
         builder_options: [
@@ -12,9 +13,25 @@ class FooterCopyrightOptionPlugin extends Plugin {
                 selector: ".o_footer_copyright",
                 editableOnly: false,
                 groups: ["website.group_website_designer"],
+                props: {
+                    getCurrentActiveViews: () => this.dependencies.HeaderNavbarOptionPlugin.getCurrentActiveViews(this.keys),
+                },
             }
         ],
     };
+
+    setup() {
+        this.keys = [
+            "website.footer_custom",
+            "website.template_footer_descriptive",
+            "website.template_footer_centered",
+            "website.template_footer_links",
+            "website.template_footer_minimalist",
+            "website.template_footer_contact",
+            "website.template_footer_call_to_action",
+            "website.template_footer_headline",
+        ];
+    }
 }
 
 registry
