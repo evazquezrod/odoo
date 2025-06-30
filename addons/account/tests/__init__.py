b6418e1898e520dc5f61f38f8fbaed2ec2b5e821
Mahdi Alijani (malj) <malj@odoo.com>
2025-06-30 17:15:16 +0200

    [FIX] mail, account: fix receive email in account alias from a user without access to the company
    To reproduce the bug:
    1- Create a DB with two companies and accounting app
    2- Create a user and allow it to access company 2
    3- Send a email using the user email to alias from purchase
    journal alias of company 2
    4- The email will be rejected
    5- Send a email using a random email address to alias.
    6- The email will be accepted and an account.move is created.
    
    In a normal flow, when a user associated to the email not
    exists, the user_id is set to odoobot, otherwise, the user
    accosiated to the email.
    The bug happens because _compute_company_id in account_move
    model, will set company_id to empty when user has no access
    to the company, as a result the account_move will fail.
    There is not a easy fix to solve this issue in account.
    In this fix, when the user doesn't have access to the company
    the user_id will be set to odoo_bot same as the case when
    user doesn't exist in the database.
    
    opw-4853027

--- addons/account/tests/__init__.py
+++ addons/account/tests/__init__.py
@@ -58,3 +58,4 @@ from . import test_account_merge_wizard
 from . import test_account_move_attachment
 from . import test_account_move_auto_post
 from . import test_dict_to_xml
+from . import test_account_journal_alias_other_company
