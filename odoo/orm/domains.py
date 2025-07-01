75564947801983fa260428f28fc26f1ebbf4b9bd
Krzysztof Magusiak (krma) <krma@odoo.com>
2025-07-01 09:48:43 +0000

    [IMP] orm: domain check optimization
    We check the optimization level so that we avoid a harder to debug
    message down the line.

--- odoo/orm/domains.py
+++ odoo/orm/domains.py
@@ -933,8 +933,10 @@ class DomainCondition(Domain):
 
         # handle searchable fields
         if field.search and field.name == self.field_expr:
-            domain = self._optimize_field_search_method(model).optimize(model)
-            # the domain is optimized so that value data types are already comparable
+            domain = self._optimize_field_search_method(model)
+            # The domain is optimized so that value data types are comparable.
+            # Only simple optimization to avoid endless recursion.
+            domain = domain.optimize(model)
             if domain != self:
                 return domain
 
@@ -1052,6 +1054,8 @@ class DomainCondition(Domain):
         field_expr, operator, value = self.field_expr, self.operator, self.value
         assert operator in STANDARD_CONDITION_OPERATORS, \
             f"Invalid operator {operator!r} for SQL in domain term {(field_expr, operator, value)!r}"
+        assert self._opt_level >= OptimizationLevel.FULL, \
+            f"Must fully optimize before generating the query {(field_expr, operator, value)}"
 
         field = model._fields[parse_field_expr(field_expr)[0]]
         return field.condition_to_sql(field_expr, operator, value, model, alias, query)
