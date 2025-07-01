eb33800f63f079d365b4ff5b6561396163dd9499
ksar-odoo <ksar@odoo.com>
2025-07-01 19:21:48 +0530

    [IMP] hr_attendance: Update the employee status immediately on checkout or checkin in UI
    Current behavior: When a user is checked-out, his presence indicator is only
      updated if you refresh your browser. 
    
    After this commit: Status will update immediately if checkout/checkin in the ui.
    
    Task-4855213

---
+++ addons/hr_attendance/static/src/components/hr_presence_status/hr_presence_status.js
@@ -0,0 +1,27 @@
+
+import { onWillDestroy, onWillStart } from "@odoo/owl";
+import { patch } from "@web/core/utils/patch";
+import { HrPresenceStatus } from "@hr/components/hr_presence_status/hr_presence_status"
+
+patch(HrPresenceStatus.prototype, {
+    setup() {
+        super.setup();
+        this.busService = this.env.services.bus_service;
+        // this.employeeAttendanceChannel = 'hr_attendance.channel/presence';
+        this.employeeAttendanceChannel = `hr_attendance_presence:${this.props.record.resModel}:${this.props.record.resId}`;
+        onWillStart(() => {
+            this.busService.addChannel(this.employeeAttendanceChannel)
+            this.busService.subscribe('presence_status', this._update_employee_presence.bind(this));
+        })
+        onWillDestroy(() => {
+            this.busService.deleteChannel(this.employeeAttendanceChannel);
+        });
+    },
+    _update_employee_presence(payload) {
+        if (payload?.channel === this.employeeAttendanceChannel && payload?.data?.emp_id === this.props.record.resId) {
+            if(payload.data.status) {
+                this.props.record.data.hr_icon_display = payload.data.status;
+            }
+        }
+    }
+})
