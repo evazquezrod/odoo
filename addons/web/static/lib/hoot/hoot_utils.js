8810dbb2ac065d1874c016c49647a5374e910db2
Julien Mougenot <jum@odoo.com>
2025-07-02 11:29:17 +0200

    [FIX] web: Hoot - isInstanceOf
    This commit introduces an internal function to Hoot DOM utils:
    `isInstanceOf`. This helper is intended to be a cross-realm
    implementation of 'instanceof'.
    
    All uses of 'instanceof' against global objects in Hoot have been
    replaced by this helper, to make allow the system to receive externally
    defined objects and act as expected.
    
    The most notable way to see this change is to use the `toBeInstanceOf`
    matcher with an element contained within an iframe.

--- addons/web/static/lib/hoot/hoot_utils.js
+++ addons/web/static/lib/hoot/hoot_utils.js
@@ -4,6 +4,7 @@ import { queryAll } from "@odoo/hoot-dom";
 import { reactive, useEffect, useExternalListener } from "@odoo/owl";
 import { isNode } from "@web/../lib/hoot-dom/helpers/dom";
 import {
+    isInstanceOf,
     isIterable,
     parseRegExp,
     R_WHITE_SPACE,
@@ -157,7 +158,7 @@ function getFunctionString(fn) {
  */
 function getGenericSerializer(value) {
     for (const [constructor, serialize] of GENERIC_SERIALIZERS) {
-        if (value instanceof constructor) {
+        if (isInstanceOf(value, constructor)) {
             return serialize;
         }
     }
@@ -235,7 +236,7 @@ function _deepEqual(a, b, ignoreOrder, partial, cache) {
     }
 
     // Files
-    if (a instanceof File) {
+    if (isInstanceOf(a, File)) {
         // Files
         return a.name === b.name && a.size === b.size && a.type === b.type;
     }
@@ -734,13 +735,13 @@ export function deepCopy(value) {
     }
 
     if (typeof value === "object" && !Markup.isMarkup(value)) {
-        if (value instanceof String || value instanceof Number || value instanceof Boolean) {
+        if (isInstanceOf(value, String, Number, Boolean)) {
             return value;
         }
         if (isNode(value)) {
             // Nodes
             return value.cloneNode(true);
-        } else if (value instanceof Date || value instanceof RegExp) {
+        } else if (isInstanceOf(value, Date, RegExp)) {
             // Dates & regular expressions
             return new (getConstructor(value))(value);
         } else if (isIterable(value)) {
@@ -866,13 +867,13 @@ export function ensureArray(value) {
  * @returns {Error}
  */
 export function ensureError(value) {
-    if (value instanceof Error) {
+    if (isInstanceOf(value, Error)) {
         return value;
     }
-    if (value instanceof ErrorEvent) {
+    if (isInstanceOf(value, ErrorEvent)) {
         return ensureError(value.error || value.message);
     }
-    if (value instanceof PromiseRejectionEvent) {
+    if (isInstanceOf(value, PromiseRejectionEvent)) {
         return ensureError(value.reason || value.message);
     }
     return new Error(String(value || "unknown error"));
@@ -1044,19 +1045,19 @@ export function getTypeOf(value) {
             if (value === null) {
                 return "null";
             }
-            if (value instanceof Date) {
+            if (isInstanceOf(value, Date)) {
                 return "date";
             }
-            if (value instanceof Error) {
+            if (isInstanceOf(value, Error)) {
                 return "error";
             }
             if (isNode(value)) {
                 return "node";
             }
-            if (value instanceof RegExp) {
+            if (isInstanceOf(value, RegExp)) {
                 return "regex";
             }
-            if (value instanceof URL) {
+            if (isInstanceOf(value, URL)) {
                 return "url";
             }
             if ($isArray(value)) {
@@ -1116,17 +1117,17 @@ export function isOfType(value, type) {
         case "any":
             return true;
         case "date":
-            return value instanceof Date;
+            return isInstanceOf(value, Date);
         case "error":
-            return value instanceof Error;
+            return isInstanceOf(value, Error);
         case "integer":
             return $isInteger(value);
         case "node":
             return isNode(value);
         case "regex":
-            return value instanceof RegExp;
+            return isInstanceOf(value, RegExp);
         case "url":
-            return value instanceof URL;
+            return isInstanceOf(value, URL);
         default:
             return typeof value === type;
     }
@@ -1284,7 +1285,7 @@ export function match(value, ...matchers) {
     }
     return matchers.some((matcher) => {
         if (typeof matcher === "function") {
-            if (value instanceof matcher) {
+            if (isInstanceOf(value, matcher)) {
                 return true;
             }
             matcher = new RegExp(matcher.name);
@@ -1293,7 +1294,7 @@ export function match(value, ...matchers) {
         if (R_OBJECT.test(strValue)) {
             strValue = getConstructor(value).name;
         }
-        if (matcher instanceof RegExp) {
+        if (isInstanceOf(matcher, RegExp)) {
             return matcher.test(strValue);
         } else {
             return strValue.includes(String(matcher));
@@ -1346,7 +1347,7 @@ export function parseQuery(query) {
         return [];
     }
     const regex = parseRegExp(nQuery, { safe: true });
-    if (regex instanceof RegExp) {
+    if (isInstanceOf(regex, RegExp)) {
         // Do not go further: the entire query is treated as a regular expression
         return [new QueryRegExp(regex)];
     }
@@ -1541,7 +1542,7 @@ export class Callbacks {
      * @param {boolean} [once]
      */
     add(type, callback, once) {
-        if (callback instanceof Promise) {
+        if (isInstanceOf(callback, Promise)) {
             const promiseValue = callback;
             callback = function waitForPromise() {
                 return Promise.resolve(promiseValue).then(resolve);
