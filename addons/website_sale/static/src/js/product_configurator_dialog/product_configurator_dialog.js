4144a425d31f5a5a3937517175fee54accfdedda
Chrysanthe (chgo) <chgo@odoo.com>
2025-07-03 08:40:02 +0200

    fixup! [IMP] website_sale: redesigned UI in product configurator dialog

--- addons/website_sale/static/src/js/product_configurator_dialog/product_configurator_dialog.js
+++ addons/website_sale/static/src/js/product_configurator_dialog/product_configurator_dialog.js
@@ -30,11 +30,7 @@ patch(ProductConfiguratorDialog.prototype, {
             this.createProductUrl = '/website_sale/product_configurator/create_product';
             this.updateCombinationUrl = '/website_sale/product_configurator/update_combination';
             this.getOptionalProductsUrl = '/website_sale/product_configurator/get_optional_products';
-            // To be translated, the title must be repeated here. Indeed, only
-            // translations of "frontend modules" are fetched in the context of
-            // website. The original definition of the title is in "sale", which
-            // is not a frontend module.
-            this.title = _t(" ");
+            this.title = "";
         }
 
         useSubEnv({
