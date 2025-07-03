d600edfaf511f5fb5935ae6858c40043cc4aaf57
Abdullah (abye) <abye@odoo.com>
2025-07-03 11:51:33 +0200

    [ADD] crm_project: convert opportunity into a project
    - show the stat button only with project access level
    - create a helper for the linked projects page
    
    task-4847853

--- addons/crm_project/models/crm_lead.py
+++ addons/crm_project/models/crm_lead.py
@@ -6,12 +6,12 @@ from odoo.tools.translate import _
 
 
 class CrmLead(models.Model):
-    _inherit = 'crm.lead'
+    _inherit = "crm.lead"
 
-    linked_project_ids = fields.One2many('project.project', inverse_name='lead_id', help="Projects linked to this lead.")
-    linked_project_count = fields.Integer(compute='_compute_linked_project_count', help="Number of projects linked to this lead.")
+    linked_project_ids = fields.One2many("project.project", inverse_name="lead_id", help="Projects linked to this lead.")
+    linked_project_count = fields.Integer(compute="_compute_linked_project_count", help="Number of projects linked to this lead.")
 
-    @api.depends('linked_project_ids')
+    @api.depends("linked_project_ids")
     def _compute_linked_project_count(self):
         for lead in self:
             lead.linked_project_count = len(lead.linked_project_ids)
@@ -20,22 +20,24 @@ class CrmLead(models.Model):
         self.ensure_one()
 
         action = {
-            'type': 'ir.actions.act_window',
-            'name': _('Lead Projects'),
-            'res_model': 'project.project',
-            'view_mode': 'kanban,form',
-            'context': dict(self._context, default_company_id=self.company_id.id),
-            'domain': [('id', 'in', self.linked_project_ids.ids)],
-            'help': """
-                <div class="container mt64">
-                    <h1>No Projects found.</h1>
-                    <p class="lead">Create a new one from scratch.</p>
-                </div>
+            "type": "ir.actions.act_window",
+            "name": _("Lead Projects"),
+            "res_model": "project.project",
+            "view_mode": "kanban,form",
+            "context": dict(self.env.context, default_company_id=self.company_id.id),
+            "domain": [("id", "in", self.linked_project_ids.ids)],
+            "help": """
+                <p class="o_view_nocontent_smiling_face">
+                    No projects found. Let"s create one!
+                </p>
+                <p>
+                    Create projects to organize your tasks. Define a different workflow for each project.
+                </p>
             """,
         }
         if self.linked_project_count == 1:
             action.update({
-                'view_mode': 'form',
-                'res_id': self.linked_project_ids.id,
+                "view_mode": "form",
+                "res_id": self.linked_project_ids.id,
             })
         return action
