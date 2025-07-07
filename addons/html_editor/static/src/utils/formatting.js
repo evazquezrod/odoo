0a0cfc373fe5ba1f0382245ad1d53eefd97d05d2
Robin Lejeune (role) <role@odoo.com>
2025-07-07 13:42:05 +0200

    [FIX] html_editor: update font size of headings with fs class
    Steps to reproduce:
    - In the website builder, drop a snippet with a heading that has a
    font-size class (e.g. `<h2 class="h4-fs">`). For instance "Key benefits"
    - Select the text of the heading and try to change its size
    => It doesn't work. The class remains, and no span child is created.
    
    Until [1], in such a case, a span child was created, but the original
    class was not removed. This is a best effort with the current editor:
    ideally, the new class should be applied on the heading tag itself (to
    avoid weird line heights).
    The behavior was introduced with [2] but was not replicated in the
    html_editor refactoring [3], which did not need it at that time.
    
    [1]: https://github.com/odoo/odoo/commit/9fe45e2b7ddbbfd0445ffe25a859e67a316d02b2
    [2]: https://github.com/odoo/odoo/commit/194f73a9bbad8c3c3fb5c378e7bdfa704aaacdc0
    [3]: https://github.com/odoo/odoo/commit/97117e70a1766ad24f5ec14ffef303127d56b76b
    
    task-4367641

--- addons/html_editor/static/src/utils/formatting.js
+++ addons/html_editor/static/src/utils/formatting.js
@@ -93,7 +93,9 @@ export const formatsSpecs = {
         removeStyle: (node) => removeStyle(node, "font-family"),
     },
     fontSize: {
-        isFormatted: (node) => closestElement(node)?.style["font-size"],
+        isFormatted: (node, props) =>
+            closestElement(node)?.style["font-size"] &&
+            closestElement(node).style["font-size"] === props.size,
         hasStyle: (node) => node.style && node.style["font-size"],
         addStyle: (node, props) => {
             node.style["font-size"] = props.size;
@@ -102,9 +104,10 @@ export const formatsSpecs = {
         removeStyle: (node) => removeStyle(node, "font-size"),
     },
     setFontSizeClassName: {
-        isFormatted: (node) =>
-            FONT_SIZE_CLASSES.find((cls) => closestElement(node)?.classList?.contains(cls)),
-        hasStyle: (node, props) => FONT_SIZE_CLASSES.find((cls) => node.classList.contains(cls)),
+        isFormatted: (node, props) =>
+            FONT_SIZE_CLASSES.includes(props.className) &&
+            closestElement(node)?.classList.contains(props.className),
+        hasStyle: (node) => FONT_SIZE_CLASSES.find((cls) => node.classList.contains(cls)),
         addStyle: (node, props) => {
             node.style.removeProperty("font-size");
             node.classList.add(props.className);
